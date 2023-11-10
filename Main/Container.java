21463b68020f348683eed455ba258a0331a9e919
deivi <132165775+Davicbm@users.noreply.github.com>
2023-11-09 22:24:25 -0300

    Update Container.java

--- Main/Container.java
+++ Main/Container.java
@@ -1,64 +1,21 @@
-package meujogo.modelo;
+package meujogo;
 
-import java.awt.Image;
-import java.awt.Rectangle;
+import javax.swing.JFrame;
 
-import javax.swing.ImageIcon;
+import meujogo.modelo.Fase;
 
-public class Inimigo1 {
-	private Image imagem;
-	private int x;
-	private int y;
-	private int largura;
-	private int altura;
-	private boolean isVisivel;
+public class Container extends JFrame{
 	
-	//private static final int LARGURA = 938;
-	private static int VELOCIDADE = 5;
-	
-	public Inimigo1(int x, int y) {
-		this.x = x;
-		this.y = y;
-		isVisivel = true;
+	public Container() {
+		add(new Fase());
+		setTitle ("Br-Callister");
+		setSize(1024,728);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		this.setResizable(false);
+		setVisible(true);
 	}
-	public void load() {
-		ImageIcon referencia = new ImageIcon("res\\robo2.png");
-		imagem = referencia.getImage();
-		
-		this.largura = imagem.getWidth(null);
-		this.altura = imagem.getHeight(null);
+	public static void main(String[]args) {
+		new Container ();
 	}
-	public void update() {
-		this.x -= VELOCIDADE;
-		
-		//if(this.x > LARGURA) {
-			//isVisivel = false;
-		//}
-	}
-	public Rectangle getBounds() {
-		return new Rectangle(x,y,largura,altura);
-	}
-	public boolean isVisivel() {
-		return isVisivel;
-	}
-	public void setVisivel(boolean isVisivel) {
-		this.isVisivel = isVisivel;
-	}
-	public static int getVELOCIDADE() {
-		return VELOCIDADE;
-	}
-	public static void setVELOCIDADE(int vELOCIDADE) {
-		VELOCIDADE = vELOCIDADE;
-	}
-	public int getX() {
-		return x;
-	}
-	public int getY() {
-		return y;
-	}
-	public Image getImagem() {
-		return imagem;
-	}
-	
-	
 }
