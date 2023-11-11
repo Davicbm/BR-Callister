70a92b03b8592aae0415a35a94bebd6cf3903eb1
biancanol <biadnasc@gmail.com>
2023-11-11 13:22:38 -0300

    gif do robô

--- Jogo/Componentes/Inimigos/Robo.java
+++ Jogo/Componentes/Inimigos/Robo.java
@@ -21,7 +21,7 @@ public class Robo {
 		isVisivel = true;
 	}
 	public void load() {
-		ImageIcon referencia = new ImageIcon("assets//robo.png");
+		ImageIcon referencia = new ImageIcon("assets//robo.gif");
 		imagem = referencia.getImage();
 		
 		this.largura = imagem.getWidth(null);
