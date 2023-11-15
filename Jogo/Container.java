1528eca1aa55405aaa30db67fddc55454f1c10f9
Davicbm <davicarvalho.b1@gmail.com>
2023-11-14 22:10:46 -0300

    menu adicionado

--- Jogo/Container.java
+++ Jogo/Container.java
@@ -1,20 +1,38 @@
 package Jogo;
 
 import javax.swing.JFrame;
+import javax.swing.Timer;
 
 import Jogo.Componentes.Fase;
+import Jogo.Componentes.MenuPrincipal;
 
 public class Container extends JFrame {
+	MenuPrincipal menu = new MenuPrincipal();
 
 	public Container() {
-		add(new Fase());
 		setTitle("Br-Callister");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		this.setResizable(false);
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
 		setVisible(true);
-	}
+
+		add(menu);
+		checkGameStatus();
+    }
+
+    private void checkGameStatus() {
+        Timer timer = new Timer(100, e -> {
+            if (menu.isInicioJogo()) {
+                remove(menu);
+                add(new Fase());
+                revalidate(); // Atualiza o JFrame para refletir as mudanças
+                repaint(); // Redesenha o JFrame
+                ((Timer) e.getSource()).stop(); // Para o timer, já que o jogo começou
+            }
+        });
+        timer.start();
+    }
 	public static void main(String[] args) {
 		new Container();
 	}
